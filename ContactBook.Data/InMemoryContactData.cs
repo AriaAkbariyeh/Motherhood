726f58d82eb5831d47cce39a5a83229dc988dc5c
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-07 04:10:23 +0800

    Add InMemoryContactData class

---
+++ ContactBook.Data/InMemoryContactData.cs
@@ -0,0 +1,31 @@
+﻿using ContactBook.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContactBook.Data
+{
+    public class InMemoryContactData : IContactData
+    {
+        List<Contact> contacts;
+
+        public InMemoryContactData()
+        {
+            contacts = new List<Contact>
+            {
+                new Contact { ID = 1, Name = "Aria Ak", Address="Wangsa Maju Av.", Nationality=NationalityType.Iranian, Number="01167702424" },
+                new Contact { ID = 2, Name = "Bob Doe", Address="WallStreet", Nationality=NationalityType.Russian, Number="01812803034" },
+                new Contact { ID = 3, Name = "Alice Marley", Address="Intermark", Nationality=NationalityType.Polish, Number="01754541001" }
+                new Contact { ID = 4, Name = "Jacky Chan", Address="Menara Binjai", Nationality=NationalityType.Chinese, Number="0123881280" }
+            };
+        }
+
+        public IEnumerable<Contact> GetAll()
+        {
+            return from c in contacts
+                   orderby c.Name
+                   select c;
+        }
+
+    }
+
+}
