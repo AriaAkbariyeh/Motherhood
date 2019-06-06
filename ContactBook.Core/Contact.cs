bde4ce65b8e493fb7b5d6aa843e241debd27dc80
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-07 04:11:50 +0800

    Change ID property of contact to Id

--- ContactBook.Core/Contact.cs
+++ ContactBook.Core/Contact.cs
@@ -2,7 +2,7 @@
 {
     public class Contact
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
         public string Address { get; set; }
--- ContactBook.Data/InMemoryContactData.cs
+++ ContactBook.Data/InMemoryContactData.cs
@@ -12,10 +12,10 @@ namespace ContactBook.Data
         {
             contacts = new List<Contact>
             {
-                new Contact { ID = 1, Name = "Aria Ak", Address="Wangsa Maju Av.", Nationality=NationalityType.Iranian, Number="01167702424" },
-                new Contact { ID = 2, Name = "Bob Doe", Address="WallStreet", Nationality=NationalityType.Russian, Number="01812803034" },
-                new Contact { ID = 3, Name = "Alice Marley", Address="Intermark", Nationality=NationalityType.Polish, Number="01754541001" }
-                new Contact { ID = 4, Name = "Jacky Chan", Address="Menara Binjai", Nationality=NationalityType.Chinese, Number="0123881280" }
+                new Contact { Id = 1, Name = "Aria Ak", Address="Wangsa Maju Av.", Nationality=NationalityType.Iranian, Number="01167702424" },
+                new Contact { Id = 2, Name = "Bob Doe", Address="WallStreet", Nationality=NationalityType.Russian, Number="01812803034" },
+                new Contact { Id = 3, Name = "Alice Marley", Address="Intermark", Nationality=NationalityType.Polish, Number="01754541001" }
+                new Contact { Id = 4, Name = "Jacky Chan", Address="Menara Binjai", Nationality=NationalityType.Chinese, Number="0123881280" }
             };
         }
 
