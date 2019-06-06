029d60ec870de5a53c2901abcf21ab089fdf5cd8
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-07 04:27:19 +0800

    Inject IcontactData

--- ContactBook.Application/Pages/Contacts/List.cshtml.cs
+++ ContactBook.Application/Pages/Contacts/List.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContactBook.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,12 @@ namespace ContactBook.Application.Pages.Contacts
 {
     public class ListModel : PageModel
     {
+        private readonly IContactData contactData;
+
+        public ListModel(IContactData contactData)
+        {
+            this.contactData = contactData;
+        }
         public void OnGet()
         {
         }
