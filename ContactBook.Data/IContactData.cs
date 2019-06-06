58a61954e2c663d51ed691ddcd77f4bc0f58c1ac
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-07 04:00:25 +0800

    Update IContactData Interface

--- ContactBook.Data/IContactData.cs
+++ ContactBook.Data/IContactData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ContactBook.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace ContactBook.Data
 {
     public interface IContactData
     {
-        
+        IEnumerable<Contact> GetAll();
     }
+
 }
