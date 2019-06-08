3669de5c464d04cddc5c837ab269c135f5674d78
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-08 19:22:24 +0800

    Implement SqlContactData

---
+++ ContactBook.Data/SqlContactData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ContactBook.Core;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBook.Data
+{
+    class SqlContactData : IContactData
+    {
+        private readonly ContactBookSqlDbContext db;
+
+        public SqlContactData(ContactBookSqlDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Contact Add(Contact contact)
+        {
+            db.contacts.Add(contact);
+            return contact;
+        }
+
+        public int Commit()
+        {
+            return db.SaveChanges();
+        }
+
+        public Contact Delete(int Id)
+        {
+            var contact = GetContactById(Id);
+            if(contact != null)
+            {
+                db.contacts.Remove(contact);
+            }
+
+            return contact;
+        }
+
+        public Contact GetContactById(int Id)
+        {
+            return db.contacts.Find(Id);
+        }
+
+        public IEnumerable<Contact> GetContactsByName(string name)
+        {
+            var query = from c in db.contacts
+                        where c.Name.StartsWith(name) || string.IsNullOrEmpty(name)
+                        orderby c.Name
+                        select c;
+            return query;
+        }
+
+        public Contact Update(Contact updatedContact)
+        {
+            var entity = db.contacts.Attach(updatedContact);
+            entity.State = EntityState.Modified;
+            return updatedContact;
+
+        }
+    }
+}
