4861fd077707c456e002a09953b21e049c129940
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-08 18:36:01 +0800

    Create SqlDbContext class

---
+++ ContactBook.Data/ContactBookSqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactBook.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactBook.Data
+{
+    class ContactBookSqlDbContext : DbContext
+    {
+        public DbSet<Contact> contacts{ get; set; }
+
+
+    }
+}
