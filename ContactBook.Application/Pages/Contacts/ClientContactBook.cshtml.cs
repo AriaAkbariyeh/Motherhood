f8e1db268a60049a0419a73e8ccb70e2470c25eb
AriaAkbariyeh <aria.ak@hotmail.com>
2019-06-11 19:55:23 +0800

    Add API controller and a concumer page

---
+++ ContactBook.Application/Pages/Contacts/ClientContactBook.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactBook.Application.Pages.Contacts
+{
+    public class ClientContactBookModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
