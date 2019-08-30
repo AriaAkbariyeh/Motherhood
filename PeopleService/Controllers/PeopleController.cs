79779e0dce929493bda365328c981418b5b845f3
AriaAkbariyeh <aria.ak@hotmail.com>
2019-08-31 05:18:39 +0800

    Add People webservice

---
+++ PeopleService/Controllers/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PeopleService.Models;
+
+namespace PeopleService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IPeopleProvider peopleProvider;
+
+        public PeopleController(IPeopleProvider peopleProvider)
+        {
+            this.peopleProvider = peopleProvider;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return peopleProvider.GetPeople();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return peopleProvider.GetPeople().FirstOrDefault(p => p.Id == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PeopleService/Models/IPeopleProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleService.Models
+{
+    public interface IPeopleProvider
+    {
+        List<Person> GetPeople();
+    }
+}
